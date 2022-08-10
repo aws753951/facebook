b3cfbccf2a98f280160085b7296d1e3b0f0ebf15
Marc <a0983204766@gmail.com>
2022-08-10 11:36:57 +0800

    add joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,25 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const registerValidation = require("../validation").registerValidation;
+const loginValidation = require("../validation").loginValidation;
 
 // Register
 router.post("/register", async (req, res) => {
-  // create new user
-  const newUser = new User(req.body);
+  // check input before querying
+  let { username, email, password } = req.body;
+  const { error } = registerValidation({ username, email, password });
+  if (error) return res.status(400).json(error.details[0].message);
 
-  // save user and response
   try {
+    // check email
+    const foundUser = await User.findOne({ email });
+    if (foundUser) return res.status(400).json("email has been registered.");
+
+    // create new user
+    const newUser = new User({ username, email, password });
+
+    // save user and response
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
   } catch (err) {
@@ -18,19 +29,21 @@ router.post("/register", async (req, res) => {
 
 // login
 router.post("/login", async (req, res) => {
-  let { username, email, password } = req.body;
+  // check input before querying
+  let { email, password } = req.body;
+  const { error } = loginValidation({ email, password });
+  if (error) return res.status(400).json(error.details[0].message);
+
   try {
-    const foundUser = await User.findOne({ email });
     // check email
-    if (!foundUser) {
-      res.status(400).json("User or password wrong");
-    } else {
-      // check password
-      const checkPassword = await bcrypt.compare(password, foundUser.password);
-      !checkPassword
-        ? res.status(400).json("User or password wrong")
-        : res.status(200).json(foundUser);
-    }
+    const foundUser = await User.findOne({ email });
+    if (!foundUser) return res.status(400).json("User or password wrong");
+
+    // check password
+    const checkPassword = await bcrypt.compare(password, foundUser.password);
+    !checkPassword
+      ? res.status(400).json("User or password wrong")
+      : res.status(200).json(foundUser);
   } catch (err) {
     res.status(500).json(err);
   }
